3ce3a7fc9345d3f2c5fcc929343a7848637aa622
mwiko <mwikosinyangwe@gmail.com>
2025-05-12 13:09:16 +0200

    second commit

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,23 +2,43 @@ import React, {useState} from 'react'
 
 export default function Counter() {
     const [count, setCount] = useState(0);
+    const [incrementAmount, setIncrementAmount] = useState('1');
 
     const handleIncreament = () => {
-        setCount(count + 1);
+        setCount(count + Number(incrementAmount));
     } 
     
-        const handleDecreament = () => {
+    const handleDecreament = () => {
             if (count > 0) {
               setCount(count - 1);  
             }
             
-        }
+    }
+
+    const handleReset = () => {
+    setCount(0);
+    };
+
+    const handleKeyDown = (e) => {
+    if (e.key === 'ArrowUp') {
+      handleIncreament();
+    } else if (e.key === 'ArrowDown') {
+      handleDecreament();
+    }
+  };
     
   return (
     <div>
-      <h1>Count: {count}</h1>
+      <h1 style={{ color: count % 2 === 0 ? 'green' : 'red' }}>Count: {count}</h1>
+      <input
+        type="number"
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
       <button onClick={handleIncreament}>increament</button>
       <button onClick={handleDecreament}>decreament</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   )
 }
